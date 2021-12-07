76c36f82e5e348b2308abf762dab52d5a3970778
LuisCruz <luis1016@live.com.mx>
2021-12-06 23:56:38 -0600

    home updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ function App() {
   return (
     <div className="App">
           <div id="root"></div>
-      <header className="App-header">
         <p>
           !Welcome to CubicApp Team!
         </p>
-      </header>
     </div>
   );
 }
