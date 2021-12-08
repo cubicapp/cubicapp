b6b31e26d3e95f114d7054fa1ac5a07aefa6a726
LuisCruz <luis1016@live.com.mx>
2021-12-08 00:34:05 -0600

    Updates

---
+++ static/js/main.5bbc7fe4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcubicapp=this.webpackJsonpcubicapp||[]).push([[0],{10:function(c,e,t){"use strict";t.r(e);var n=t(1),i=t.n(n),p=t(3),r=t.n(p),s=(t(8),t(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("p",{children:"!Welcome to CubicApp Team! Main"})})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(c,e,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.5bbc7fe4.chunk.js.map
